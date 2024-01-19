5f7e17c46d902b7a675d31835d72004042342df6
Datzu712 <41zohan@gmail.com>
2024-01-19 04:56:18 -0600

    Singleton logger service that use consoleLogger (but can be changed for other logger)

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export * from './console-logger';
+export * from './interfaces';
+export * from './logger.service';
---
+++ src/logger.service.ts
@@ -0,0 +1,108 @@
+import { ConsoleLogger, ConsoleLoggerOptions } from './console-logger';
+import type { LogLevels, logMessage } from './interfaces';
+
+export interface abstractLogger {
+    debug: (message: logMessage, context?: string) => void;
+    error: (message: logMessage, context?: string) => void;
+    warn: (message: logMessage, context?: string) => void;
+    log: (message: logMessage, context?: string) => void;
+    verbose: (message: logMessage, context?: string) => void;
+}
+
+const defaultLogger = new ConsoleLogger();
+
+export class Logger implements abstractLogger {
+    private static staticInstance?: abstractLogger;
+    protected localInstanceRef?: abstractLogger;
+    protected static logLevels: LogLevels[];
+
+    constructor(
+        private defaultContext?: string,
+        instanceOptions?: ConsoleLoggerOptions,
+    ) {
+        // If theres not a static instance it means that the logger is not initialized, so if we don't have the "instanceOptions" to create a new instance of "ConsoleLogger", we will use the default logger.
+        if (!Logger.staticInstance && !instanceOptions) {
+            Logger.staticInstance = defaultLogger;
+
+            // But if "instanceOptions" was provided, we will use it to create the new (and unique instance of ConsoleLogger).
+        } else if (instanceOptions && !Logger.staticInstance) {
+            Logger.staticInstance = new ConsoleLogger({ ...instanceOptions, context: defaultContext });
+        }
+        // Local instance for this instance of the logger.
+        this.localInstanceRef = Logger.staticInstance;
+    }
+
+    // Public methods
+    /**
+     * Write an 'log' message.
+     */
+    public log(message: logMessage, context = this.defaultContext) {
+        this.localInstanceRef?.log(message, context);
+    }
+    /**
+     * Write an 'error' message.
+     */
+    public error(message: logMessage, context = this.defaultContext) {
+        this.localInstanceRef?.error(message, context);
+    }
+    /**
+     * Write an 'debug' message.
+     */
+    public debug(message: logMessage, context = this.defaultContext) {
+        this.localInstanceRef?.debug(message, context);
+    }
+    /**
+     * Write an 'verbose' message.
+     */
+    public verbose(message: logMessage, context = this.defaultContext) {
+        this.localInstanceRef?.verbose(message, context);
+    }
+    /**
+     * Write an 'warn' message.
+     */
+    public warn(message: logMessage, context = this.defaultContext) {
+        this.localInstance?.warn(message, context);
+    }
+
+    // Static methods
+
+    /**
+     * Write an 'log' message.
+     */
+    static log(message: logMessage, context?: string) {
+        this.staticInstance?.log(message, context);
+    }
+    /**
+     * Write an 'error' message.
+     */
+    static error(message: logMessage, context?: string) {
+        this.staticInstance?.error(message, context);
+    }
+    /**
+     * Write an 'debug' message.
+     */
+    static debug(message: logMessage, context?: string) {
+        this.staticInstance?.debug(message, context);
+    }
+
+    /**
+     * Write an 'verbose' message.
+     */
+    static verbose(message: logMessage, context?: string) {
+        this.staticInstance?.verbose(message, context);
+    }
+    /**
+     * Write an 'warn' message.
+     */
+    static warn(message: logMessage, context?: string) {
+        this.staticInstance?.warn(message, context);
+    }
+
+    get localInstance() {
+        return this.localInstanceRef;
+    }
+
+    static overrideLocalInstance(instance: ConsoleLogger | abstractLogger) {
+        Logger.staticInstance = instance;
+    }
+}
